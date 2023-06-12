91793e262d1d340087fcaf759db5955b9913fc24
Daniel Rodriguez <kungary04@gmail.com>
2023-06-12 15:55:04 -0500

    Desing Mobile

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,23 +8,23 @@ const Form = ({ submit, typeForm }) => {
     <form onSubmit={handleSubmit(submit)} id="form" className="padding">
       <h3>{typeForm}</h3>
       <div className="inputs">
-        <label htmlFor="nameId">First Name: </label> <br />
+        <label htmlFor="nameId">Nombres: </label> <br />
         <input type="text" id="nameId" {...register('first_name')} required />
       </div>
       <div className="inputs">
-        <label htmlFor="lastNameId">Last Name:</label> <br />
+        <label htmlFor="lastNameId">Apellidos:</label> <br />
         <input type="text" id="lastNameId" {...register('last_name')} required />
       </div>
       <div className="inputs">
-        <label htmlFor="mailId">Email: </label> <br />
+        <label htmlFor="mailId">Correo: </label> <br />
         <input type="email" id="mailId" {...register('email')} />
       </div>
       <div className="inputs">
-        <label htmlFor="passwordId">Password: </label> <br />
+        <label htmlFor="passwordId">Contraseña: </label> <br />
         <input type="password" id="passwordId" {...register('password')} required />
       </div>
       <div className="inputs">
-        <label htmlFor="">Birthday: </label> <br />
+        <label htmlFor="">Cumpleaños: </label> <br />
         <input type="date" id="" {...register('birthday')} required />
       </div>
       <div className="button_submit">
